fe0f169cd4ddaa2e48f1473321fa4cba275d2787
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-08-24 23:35:03 +0300

    emailform ui design update

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -28,50 +28,68 @@ const EmailForm = () => {
     }
   };
   return (
-    <div>
+    <div className=" flex flex-col   ">
+      <div
+        className="flex justify-center items-center m-2
+      "
+      >
+        <button
+          className="text-center 
+       bg-blue-500 w-48 px-7 py-3
+       text-white
+        text-2xl cursor-text "
+        >
+          Enquiries
+        </button>
+      </div>
+
       <form
         ref={formRef}
-        className="flex flex-col items-center gap-4"
+        className="flex flex-col items-center  "
         onSubmit={sendEmail}
       >
-        <div className="flex flex-col ">
-          <label className="text-3xl" htmlFor="name">
+        <div className="flex flex-col md:w-1/2 ">
+          <label className="text-xl" htmlFor="name">
             Name
           </label>
           <input
             type="name"
             name="user_name"
             id="name"
-            className="border-black border-2 p-2  w-full"
+            placeholder="Name...."
+            className="border-black border-2 p-2  w-full  rounded-md  lg:py-5"
             // onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className="flex flex-col ">
-          <label className="text-3xl" htmlFor="email">
+        <div className="flex flex-col md:w-1/2 mt-4  md:mt-4">
+          <label className="text-xl" htmlFor="email">
             Email
           </label>
           <input
+            placeholder="Email...."
             type="email"
             name="user_email"
             id="email"
-            className="border-black border-2  p-2  w-full"
+            className="border-black border-2  p-2  w-full  rounded-md lg:py-5 "
           />
-          <label className="text-3xl" htmlFor="message">
+        </div>
+        <div className=" w-48 md:w-1/2 md:h-2/3 mt-4 md:mt-4">
+          <label className="text-xl" htmlFor="message">
             Message
           </label>
-
           <textarea
             name="message"
             id="message"
-            className="border-black border-2  p-2 py-6 w-full"
+            placeholder="Message...."
+            className="border-black border-2 rounded-md  p-2 py-6 w-full md:w-full md:h-48"
           />
         </div>
 
-        <div>
+        <div className="w-20 mb-3 md:w-40  md:my-5">
           <button
             type="submit"
             value="send"
-            className="bg-green-300 p-2 rounded-md"
+            className="bg-green-300 p-2 rounded-md  w-full md:h-1/3 text-xl"
           >
             send
           </button>
